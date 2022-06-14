167c970dbf1eaad09692407a48fd5ff7eb63a3bf
Xavior88 <107013311+Xavior88@users.noreply.github.com>
2022-06-14 21:38:11 +0700

    Update config.js

--- config.js
+++ config.js
@@ -18,11 +18,11 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6283167714830','6281382420312','6288292024190']
-global.premium = ['6288292024190']
-global.packname = 'Hisoka Morrow'
+global.owner = ['6281908052908']
+global.premium = ['6281908052908']
+global.packname = 'SychoBot'
 global.author = 'WhatsApp Bot'
-global.sessionName = 'hisoka'
+global.sessionName = 'xavior'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
 global.mess = {
